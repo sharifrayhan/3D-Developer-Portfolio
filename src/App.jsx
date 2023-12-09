fe463b08640de94aa6d7ef8143f8bf981f965dd6
sharifrayhan <sharifrayhan.official@gmail.com>
2023-12-09 20:10:22 +0600

    created routes with browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import { BrowserRouter } from "react-router-dom"
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components"
 
 
 
@@ -6,9 +7,23 @@ const App = () => {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+        <div className=" bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar></Navbar>
+            <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
